7db0e78bf10d667f3cf61bad473d9ce383ec55ba
Pavel Tolstoy <eshikvtumanebest@gmail.com>
2015-04-18 10:41:05 +1000

    Repare modal window

--- static/email_builder/_scripts/constructor_init.js
+++ static/email_builder/_scripts/constructor_init.js
@@ -73,7 +73,7 @@ $(document).ready(function(){
 
     $('#upload_image').on('change', function(){
         //var test = $('#my_form').submit();
-        $('#model2').trigger('openModal');
+        $('#modal2').trigger('openModal');
         var fd = new FormData();
         fd.append('image', document.getElementById('upload_image').files[0]);
 
@@ -94,7 +94,7 @@ $(document).ready(function(){
                 else{
                     alert('Ошибка загрузки. ' + data[1]);
                 }
-                $('#model2').trigger('closeModal');
+                $('#modal2').trigger('closeModal');
             }
         });
     });
